2ef4107e6f518b933a5446d5c1817eceb337b66b
Mike <ml.tablet.n7@gmail.com>
2022-12-12 13:09:33 -0500

    Created a following wisp that we can attach voice lines on & Started on Jump function

---
+++ FinalProjectXRDev/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpStrength;
+
+    Rigidbody playerBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        jumpStrength = 5f;
+    }
+    public void Jump()
+    {
+        playerBody.AddForce(transform.up * jumpStrength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
